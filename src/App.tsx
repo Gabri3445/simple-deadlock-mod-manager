7d63b8c56de505f7711f8b7e33c39ae77ba22bce
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 01:11:33 +0100

    add deadlock fonts

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,11 @@ function App() {
     return (
         <main className="flex h-screen flex-col">
             <div className="w-full bg-sky-400 p-4 flex justify-between">
-                <h1 className="font-extrabold text-3xl block">Deadlock Mod Manager</h1>
+                <h1 className="text-3xl block">Deadlock Mod Manager</h1>
                 <div className="flex gap-4">
                     <Button>Load Mod</Button>
                     <Button>Select game path</Button>
+                    <Button>Options</Button>
                 </div>
             </div>
             <div className="flex flex-1">
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Button({children, onClick}: { onClick?: () => void, children?: React.ReactNode }) {
     return (
-        <button className="bg-white text-black p-1 px-2 rounded-3xl" onClick={onClick}>{children}</button>
+        <button className="bg-white text-black p-1 px-2 rounded-3xl font-retail-demo font-bold"
+                onClick={onClick}>{children}</button>
     )
 }
 
