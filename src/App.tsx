81c670f26c9a662bc2c5c4c814861b5442b0b1d7
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 07:17:42 +0100

    halve the padding between the two divs

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
             <TopBar/>
             <div className="flex flex-1">
                 {/*TODO: move these into their own components*/}
-                <div className="flex-1 bg-cream p-3">
+                <div className="flex-1 bg-cream p-3 first:pr-1.5">
                     <div className="border-3 h-full border-t-0 border-darker-cream">
                         <div className="bg-darker-cream text-white h-8.75 leading-8.75 pl-2">Unloaded Mods</div>
                     </div>
