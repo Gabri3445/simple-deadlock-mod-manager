e2b94f6b614555d20856cdc94a5c13d2afe3118e
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 06:48:14 +0100

    fix potential bug with non utf-8 path

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -10,7 +10,7 @@ pub fn get_deadlock_path() -> Result<String, String> {
     match steam_dir.find_app(DEADLOCK_APP_ID) {
         Ok(Some((deadlock, library))) => {
             let app_dir = library.resolve_app_dir(&deadlock);
-            Ok(app_dir.to_str().unwrap().to_string())
+            Ok(app_dir.to_string_lossy().into_owned())
         }
         Ok(None) => Err("Deadlock not found".to_string()),
         Err(e) => Err(format!("Failed to find Steam app: {}", e)),
