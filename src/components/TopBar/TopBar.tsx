441537c1a11d427a9122115d67769ba7cbbf5b7c
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 07:18:29 +0100

    implement the file picker frontend side

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,11 +1,23 @@
 import Button from "../Button/Button.tsx";
+import {open} from "@tauri-apps/plugin-dialog";
+import {downloadDir} from "@tauri-apps/api/path";
 
 function TopBar() {
+
+    const onLoadModClick = async (): Promise<void> => {
+        const file = await open({
+            multiple: true,
+            directory: false,
+            defaultPath: await downloadDir()
+        })
+        //TODO: pass to rust and copy to game mod folder
+    }
+
     return (
         <div className="w-full bg-topbar p-4 flex justify-between">
             <h1 className="text-3xl block font-bold">Deadlock Mod Manager</h1>
             <div className="flex gap-4">
-                <Button>Load Mod</Button>
+                <Button onClick={onLoadModClick}>Load Mod</Button>
                 <Button>Options</Button>
                 <Button>Apply</Button>
             </div>
