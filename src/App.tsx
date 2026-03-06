58e750f2c8bdd0d173cec8f9d79fd9e851462e52
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 04:26:18 +0100

    change style of mod tabs

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,17 @@ function App() {
         <main className="flex h-screen flex-col">
             <TopBar/>
             <div className="flex flex-1">
-                <div className="flex-1 bg-red-500">Unloaded</div>
-                <div className="flex-1 bg-red-500">Loaded</div>
+                {/*TODO: move these into their own components*/}
+                <div className="flex-1 bg-cream p-3">
+                    <div className="border-3 h-full border-t-0 border-darker-cream">
+                        <div className="bg-darker-cream text-white h-8.75 leading-8.75 pl-2">Unloaded Mods</div>
+                    </div>
+                </div>
+                <div className="flex-1 bg-cream p-3">
+                    <div className="border-3 h-full border-t-0 border-darker-cream">
+                        <div className="bg-darker-cream text-white h-8.75 leading-8.75 pl-2">Loaded Mods</div>
+                    </div>
+                </div>
             </div>
         </main>
     );
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -6,8 +6,8 @@ function TopBar() {
             <h1 className="text-3xl block font-bold">Deadlock Mod Manager</h1>
             <div className="flex gap-4">
                 <Button>Load Mod</Button>
-                <Button>Select game path</Button>
                 <Button>Options</Button>
+                <Button>Apply</Button>
             </div>
         </div>
     )
