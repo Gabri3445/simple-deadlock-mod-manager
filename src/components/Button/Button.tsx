41a4cffc79289efe90dc3d9dd51d377e6a01d873
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 04:20:46 +0100

    add button color transitions

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Button({children, onClick}: { onClick?: () => void, children?: React.ReactNode }) {
     return (
-        <button className="bg-white text-black p-1 px-2 rounded-3xl font-retail-demo font-bold"
+        <button
+            className="bg-white/80 hover:bg-white active:bg-gray-500 transition-colors duration-200 text-black p-1 px-2 rounded-3xl font-retail-demo font-bold "
                 onClick={onClick}>{children}</button>
     )
 }
