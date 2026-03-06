53a84e4e7d275ca0479c8751987be2f1123f821b
Jade Lopez <gabriele.ope@gmail.com>
2026-03-06 04:20:25 +0100

    added correct font

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -2,8 +2,8 @@ import Button from "../Button/Button.tsx";
 
 function TopBar() {
     return (
-        <div className="w-full bg-sky-400 p-4 flex justify-between">
-            <h1 className="text-3xl block">Deadlock Mod Manager</h1>
+        <div className="w-full bg-topbar p-4 flex justify-between">
+            <h1 className="text-3xl block font-bold">Deadlock Mod Manager</h1>
             <div className="flex gap-4">
                 <Button>Load Mod</Button>
                 <Button>Select game path</Button>
