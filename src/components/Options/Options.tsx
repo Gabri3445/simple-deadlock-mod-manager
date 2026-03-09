ffb0b795c1f89454f798425164b3d985bf638240
Jade Lopez <gabriele.ope@gmail.com>
2026-03-09 17:09:19 +0100

    add functionality to the options menu

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,7 +1,21 @@
 import {Modal} from "@mui/material";
 import Button from "../Button/Button.tsx";
+import {useEffect, useState} from "react";
+import {changePath, getDeadlockPath, loadConfigCommand} from "../../generated";
 
 function Options({isOpen, onClose}: { isOpen: boolean, onClose: () => void }) {
+    const [path, setPath] = useState("");
+    useEffect(() => {
+        loadConfigCommand().then(r => setPath(r.deadlock_path));
+    }, [])
+
+    const onApply = async () => {
+        await changePath({path: path});
+    }
+
+    const onAutoDetectClick = async () => {
+        setPath(await getDeadlockPath());
+    }
     return (
         <Modal onClose={onClose} open={isOpen}>
             <div className="absolute top-1/2 left-1/2 -translate-1/2 w-2/4 h-2/3 bg-darkBlue rounded-md">
@@ -16,16 +30,18 @@ function Options({isOpen, onClose}: { isOpen: boolean, onClose: () => void }) {
                                     Deadlock Path
                                 </label>
                                 <div className="flex">
-                                    <div className="bg-gray-800 grow mr-20"><input className="text-lg m-1 w-full"/>
+                                    <div className="bg-gray-800 grow mr-20"><input value={path} onChange={(e) => {
+                                        setPath(e.target.value)
+                                    }} className="text-lg m-1 w-full mr-1"/>
                                     </div>
-                                    <Button>
+                                    <Button onClick={onAutoDetectClick}>
                                         Auto-Detect
                                     </Button>
                                 </div>
                             </div>
                         </div>
-                        <div className="ml-auto mb-8 text-2xl">
-                            <Button>Apply</Button>
+                        <div className="ml-auto mb-8">
+                            <Button onClick={onApply}>Apply</Button>
                         </div>
                     </div>
                 </div>
