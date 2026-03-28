d466ae7cf3c3190ae4ec5b6f11937fa37a23e018
Jade Lopez <gabriele.ope@gmail.com>
2026-03-28 15:39:37 +0100

    add pub to api types

--- src-tauri/src/gamebanana_api/types.rs
+++ src-tauri/src/gamebanana_api/types.rs
@@ -2,58 +2,58 @@ use serde::Deserialize;
 use std::collections::HashMap;
 
 #[derive(Debug, Deserialize)]
-struct FilesResponse {
+pub struct FilesResponse {
     #[serde(rename = "Files().aFiles()")]
-    files: HashMap<String, FileEntry>,
+    pub files: HashMap<String, FileEntry>,
 }
 
 #[derive(Debug, Deserialize)]
-struct FileEntry {
+pub struct FileEntry {
     #[serde(rename = "_idRow")]
-    id_row: String,
+    pub id_row: String,
 
     #[serde(rename = "_sFile")]
-    file: String,
+    pub file: String,
 
     #[serde(rename = "_nFilesize")]
-    filesize: u64,
+    pub filesize: u64,
 
     #[serde(rename = "_tsDateAdded")]
-    date_added: u64,
+    pub date_added: u64,
 
     #[serde(rename = "_nDownloadCount")]
-    download_count: u64,
+    pub download_count: u64,
 
     #[serde(rename = "_sDownloadUrl")]
-    download_url: String,
+    pub download_url: String,
 
     #[serde(rename = "_sMd5Checksum")]
-    md5_checksum: String,
+    pub md5_checksum: String,
 
     #[serde(rename = "_sAnalysisState")]
-    analysis_state: String,
+    pub analysis_state: String,
 
     #[serde(rename = "_sAnalysisResult")]
-    analysis_result: String,
+    pub analysis_result: String,
 
     #[serde(rename = "_sAnalysisResultVerbose")]
-    analysis_result_verbose: String,
+    pub analysis_result_verbose: String,
 
     #[serde(rename = "_sAvState")]
-    av_state: String,
+    pub av_state: String,
 
     #[serde(rename = "_sAvResult")]
-    av_result: String,
+    pub av_result: String,
 
     #[serde(rename = "_bIsArchived")]
-    is_archived: bool,
+    pub is_archived: bool,
 
     #[serde(rename = "_bHasContents")]
-    has_contents: bool,
+    pub has_contents: bool,
 
     #[serde(rename = "_sVersion")]
-    version: String,
+    pub version: String,
 
     #[serde(rename = "_sDescription")]
-    description: String,
+    pub description: String,
 }
