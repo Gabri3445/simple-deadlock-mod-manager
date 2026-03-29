6ae9bec2674da53727f351685ff6f5061824860b
Jade Lopez <gabriele.ope@gmail.com>
2026-03-29 16:44:01 +0200

    add http plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ mod utils;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .setup(|app| {
             if let Some(proj_dirs) = ProjectDirs::from("", "sdmm", "sdmm") {
                 let config_dir = proj_dirs.config_dir().join("config.json");
