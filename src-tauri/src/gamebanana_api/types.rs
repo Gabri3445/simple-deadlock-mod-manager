97319bc84ab1b0417a7f820dff3d7ddd78bae372
Jade Lopez <gabriele.ope@gmail.com>
2026-03-29 20:45:43 +0200

    add default on response

--- src-tauri/src/gamebanana_api/types.rs
+++ src-tauri/src/gamebanana_api/types.rs
@@ -9,51 +9,51 @@ pub struct FilesResponse {
 
 #[derive(Debug, Deserialize)]
 pub struct FileEntry {
-    #[serde(rename = "_idRow")]
+    #[serde(rename = "_idRow", default)]
     pub id_row: String,
 
-    #[serde(rename = "_sFile")]
+    #[serde(rename = "_sFile", default)]
     pub file: String,
 
-    #[serde(rename = "_nFilesize")]
+    #[serde(rename = "_nFilesize", default)]
     pub filesize: u64,
 
-    #[serde(rename = "_tsDateAdded")]
+    #[serde(rename = "_tsDateAdded", default)]
     pub date_added: u64,
 
-    #[serde(rename = "_nDownloadCount")]
+    #[serde(rename = "_nDownloadCount", default)]
     pub download_count: u64,
 
-    #[serde(rename = "_sDownloadUrl")]
+    #[serde(rename = "_sDownloadUrl", default)]
     pub download_url: String,
 
-    #[serde(rename = "_sMd5Checksum")]
+    #[serde(rename = "_sMd5Checksum", default)]
     pub md5_checksum: String,
 
-    #[serde(rename = "_sAnalysisState")]
+    #[serde(rename = "_sAnalysisState", default)]
     pub analysis_state: String,
 
-    #[serde(rename = "_sAnalysisResult")]
+    #[serde(rename = "_sAnalysisResult", default)]
     pub analysis_result: String,
 
-    #[serde(rename = "_sAnalysisResultVerbose")]
+    #[serde(rename = "_sAnalysisResultVerbose", default)]
     pub analysis_result_verbose: String,
 
-    #[serde(rename = "_sAvState")]
+    #[serde(rename = "_sAvState", default)]
     pub av_state: String,
 
-    #[serde(rename = "_sAvResult")]
+    #[serde(rename = "_sAvResult", default)]
     pub av_result: String,
 
-    #[serde(rename = "_bIsArchived")]
+    #[serde(rename = "_bIsArchived", default)]
     pub is_archived: bool,
 
-    #[serde(rename = "_bHasContents")]
+    #[serde(rename = "_bHasContents", default)]
     pub has_contents: bool,
 
-    #[serde(rename = "_sVersion")]
+    #[serde(rename = "_sVersion", default)]
     pub version: String,
 
-    #[serde(rename = "_sDescription")]
+    #[serde(rename = "_sDescription", default)]
     pub description: String,
 }
