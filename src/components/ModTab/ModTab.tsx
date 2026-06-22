d23e1432d25d0532c2573dfdd3caf66775d4dcd5
Jade Lopez <gabriele.ope@gmail.com>
2026-06-22 11:47:14 +0200

    add search

--- src/components/ModTab/ModTab.tsx
+++ src/components/ModTab/ModTab.tsx
@@ -1,5 +1,8 @@
 import Mod from "../Mod/Mod.tsx";
 import {ModName} from "../../generated";
+import {SearchType, useSearchStore} from "../../stores/useSearchStore.ts";
+import {useEffect, useState} from "react";
+import Fuse from "fuse.js"
 
 export enum ModTabVariant {
     LoadedMods,
@@ -7,15 +10,57 @@ export enum ModTabVariant {
 }
 
 function ModTab({variant, mods, loading}: { variant: ModTabVariant, mods: ModName[], loading: boolean }) {
+    const {search, searchType} = useSearchStore();
+    const [filteredMods, setFilteredMods] = useState<ModName[]>([]);
+
+    useEffect(() => {
+        let list: string[] = [];
+        switch (searchType) {
+            case SearchType.FileName:
+                threshold = 0
+                list = mods.map((m) => m.file_name)
+                break;
+            case SearchType.UserName:
+                list = mods.map((m) => m.user_name)
+                break;
+        }
+        const fuse = new Fuse(list, {
+            findAllMatches: true,
+        });
+
+        const result = fuse.search(search)
+        const resultIndexes = result.map(m => m.refIndex)
+
+        setFilteredMods(resultIndexes.map(m => mods[m]))
+
+    }, [search]);
+
+    useEffect(() => {
+        setFilteredMods(mods);
+    }, [mods]);
+
+    const getTabName = (variant: ModTabVariant) => {
+        let tabName = "";
+        if (variant === ModTabVariant.LoadedMods) {
+            tabName = "Loaded Mods";
+        } else if (variant === ModTabVariant.UnloadedMods) {
+            tabName = "Unloaded Mods";
+        }
+        if (search.length > 0) {
+            tabName += ` (Results Filtered)`;
+        }
+        return tabName;
+    }
+
     return (
         <div className="flex-1 bg-cream p-3 first:pr-1.5 last:pl-1.5">
             <div className="border-3 h-full border-t-0 border-darker-cream">
                 <div
-                    className="bg-darker-cream text-white h-8.75 leading-8.75 pl-2">{variant === ModTabVariant.LoadedMods ? "Loaded Mods" : "Unloaded Mods"}</div>
+                    className="bg-darker-cream text-white h-8.75 leading-8.75 pl-2">{getTabName(variant)}</div>
                 <div className="px-2 pt-2 pb-2 grid grid-cols-3 gap-2">
                     {loading ? (
                         <></>
-                    ) : mods.map((mod) => (
+                    ) : filteredMods.map((mod) => (
                         <Mod modName={mod.user_name} key={mod.file_name} fileName={mod.file_name} variant={variant}/>
                     ))}
                 </div>
