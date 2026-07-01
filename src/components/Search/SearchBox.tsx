255636437cac8225a65a0f5cae6cc812c46ff6b3
Jade Lopez <gabriele.ope@gmail.com>
2026-07-01 08:26:54 +0200

    change placeholder based on search mode

--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -1,10 +1,23 @@
 import {ClickAwayListener} from "@mui/material";
-import {CSSProperties, Ref} from "react";
+import {CSSProperties, Ref, useEffect, useState} from "react";
 import {SearchType, useSearchStore} from "../../stores/useSearchStore.ts";
 
 
 function SearchBox({ref, style, setOpen}: { ref?: Ref<any>, style?: CSSProperties, setOpen: (open: boolean) => void }) {
     const {setSearch, search, searchType, setSearchType} = useSearchStore();
+    const [placeholder, setPlaceholder] = useState("");
+
+    useEffect(() => {
+        switch (searchType) {
+            case SearchType.UserName:
+                setPlaceholder("Enter name Here...")
+                break;
+            case SearchType.FileName:
+                setPlaceholder("Enter file number here...")
+                break;
+        }
+    }, [searchType]);
+
     return (
         <ClickAwayListener onClickAway={() => setOpen(false)}>
             <div ref={ref} style={style} className={"bg-topbar border rounded-md shadow-2xl top-2! p-4"}>
@@ -23,7 +36,7 @@ function SearchBox({ref, style, setOpen}: { ref?: Ref<any>, style?: CSSPropertie
                 <div className={"mt-4 w-full"}>
                     <input onChange={(e) => {
                         setSearch(e.target.value);
-                    }} value={search} className={"border p-2 w-full"} autoFocus placeholder={"Enter name Here..."}/>
+                    }} value={search} className={"border p-2 w-full"} autoFocus placeholder={placeholder}/>
                 </div>
             </div>
         </ClickAwayListener>
